fc91cc75de8df165cbf313693cce2d4638245402
Clay McClure <clay@daemons.net>
2022-01-27 23:34:05 -0800

    reading: Move TC++PL

---
+++ docs/programming/c++/templates.cc
@@ -0,0 +1,17 @@
+template<typename T>
+void func()
+{
+    // ...
+}
+
+template<typename T>
+class Class
+{
+    // ...
+};
+
+template<typename T>
+T var;
+
+template<typename T>
+using alias = T;
